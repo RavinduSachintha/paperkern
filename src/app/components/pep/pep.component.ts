9d70540ec9b6d20cd051940ff7097b72d08e0c55
Ravindu Sachintha <ravindusachintha53@gmail.com>
2021-08-31 17:09:33 +0530

    firebase secrets added to the app

---
+++ src/app/components/pep/pep.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pep',
+  templateUrl: './pep.component.html',
+  styleUrls: ['./pep.component.sass']
+})
+export class PepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/models/pep.spec.ts
@@ -0,0 +1,7 @@
+import { Pep } from './pep';
+
+describe('Pep', () => {
+  it('should create an instance', () => {
+    expect(new Pep()).toBeTruthy();
+  });
+});
